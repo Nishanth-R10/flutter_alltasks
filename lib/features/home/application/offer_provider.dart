2c122ef4b7994a1821d0075d061953d06e378491
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-10-02 17:32:29 +0530

    Delete lib directory

--- lib/features/home/application/offer_provider.dart
+++
@@ -1,8 +0,0 @@
-// lib/features/home/application/offer_provider.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../domain/offer_entity.dart';
-import '../infrastructure/offer_repository.dart';
-
-final offerProvider = Provider<List<OfferEntity>>((ref) {
-  return OfferRepository().getOffers();
-});
