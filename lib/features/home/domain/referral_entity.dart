e21be260b000b547257a0775a6075b3aa399457c
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-10-08 17:08:59 +0530

    Delete lib directory

--- lib/features/home/domain/referral_entity.dart
+++
@@ -1,9 +0,0 @@
-class ReferralEntity {
-  final String referralCode;
-  final int rewardPoints;
-
-  const ReferralEntity({
-    required this.referralCode,
-    required this.rewardPoints,
-  });
-}
