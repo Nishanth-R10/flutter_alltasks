76f83e192b380f05c5721eda83245e022f431d4a
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-10-16 10:59:56 +0530

    Delete lib directory

--- lib/features/home/application/user_provider.dart
+++
@@ -1,9 +0,0 @@
-// lib/core/providers/user_provider.dart
-import 'package:flutter_riverpod/legacy.dart';
-
-enum UserType {
-  newUser,
-  returningUser,
-}
-
-final userTypeProvider = StateProvider<UserType>((ref) => UserType.newUser);
