d7712ed70f5657cf115983b15c1ed299e095653c
Nishanth R <nishanthr2604@gmail.com>
2025-10-23 13:38:19 +0530

    Delete lib/features/home directory

--- lib/features/home/application/referral_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../domain/entities/referral_entity.dart';
-import '../infrastructure/referral_repository.dart';
-
-final referralProvider = Provider<ReferralEntity>((ref) {
-  return ReferralRepository().getReferralDetails();
-});
