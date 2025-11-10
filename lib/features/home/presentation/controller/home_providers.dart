92b1c5ab3730ce3098fa20e1f7d66cde2852201c
Nishant-R10 <nishanth.r@omnicoredigital.com>
2025-11-10 18:15:30 +0530

    feature(offers): keep offers feature + core + main.dart

--- lib/features/home/presentation/controller/home_providers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
-
-// Simple UI State Providers - No data, just UI state
-final selectedOfferTabProvider = StateProvider<int>((ref) => 0);
